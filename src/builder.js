f26525e34a765fe373d952aa73f2d0e0b2ab00fe
Juan Manuel <hola@jmanuelem.us>
2018-05-03 10:51:13 -0600

    Rewritten code base and internal APIs improvement

--- src/builder.js
+++ src/builder.js
@@ -1,233 +1,145 @@
-class WebForm {
+class FormBuilder
+{
+    constructor()
+    {
+        this.Toolbox = new Toolbox(); 
 
-    constructor(mame = '') {
-        this.name  = name;
-        this.pages = new Array();
-    }
-
-    add(el) {
-
-        if (el instanceof Page)
-            return this.addPage(el);
-
-        else {
-
-            if (this.pages.length == 0)
-                this.add(new Page());
-
-            return this.pages[0].add(el);
-        }
-    }
-
-    addPage(el) {
-        return this.pages.push(el);
-    }
-
-    page(id) {
-        
-        if (typeof id == 'number')
-            return this.pages[id];
-
-        else {
-
-            var obj;
-
-            this.pages.forEach(function(el) {
-                if (!obj && el.name == id) obj = el;
-            
-            }); return obj;
-        }
-    }
-
-    section(id) {
-        var obj;
-        
-        if (typeof id == 'number') {
-
-            this.pages.forEach(function(el) {
-                
-                if (!obj) {
-
-                    if (id < el.sections.length)
-                        obj = el.section(id);
-                    else
-                        id -= el.sections.length;
-                }
-
-            }); return obj; 
-
-        } else {
-
-            var section;
-
-            this.pages.forEach(function(el) {
-                if (!obj && (section = el.section(id))) obj = section;
-            
-            }); return obj;
-        }
+        this.Form    = new Form();
     }
 }
 
-class Page {
-
-    constructor(name = '') {
-        this.name     = name;
-        this.sections = new Array();
+class Toolbox
+{
+    constructor()
+    {
+        this.controls = new Array();
     }
 
-    add(el) {
-
-        if (el instanceof Section)
-            return this.addSection(el);
-
-        else {
-
-            if (this.sections.length == 0)
-                this.add(new Section());
-
-            return this.sections[0].add(el);
-        }
-    }
-
-    addSection(el) {
-        return this.sections.push(el);
-    }
-
-    section(id) {
-    
-        if (typeof id == 'number')
-            return this.sections[id];
-
-        else {
-
-            var obj;
-
-            this.sections.forEach(function(el) {
-                if (!obj && el.name == id) obj = el;
-            
-            }); return obj;
-        }
-    }
-}
-
-class Section {
-
-    constructor(name = '') {
-        this.name        = name;
-        this.subSections = new Array();
-    }
-
-    add(el) {
-
-        if (el instanceof SubSection)
-            return this.addSubSection(el);
-
-        else {
-
-            if (this.subSections.length == 0)
-                this.add(new SubSection());
-
-            return this.subSections[0].add(el);
-        }
-    }
-
-    addSubSection(el) {
-        return this.subSections.push(el);
-    }
-
-    subSection(id) {
-        // TODO
-    }
-}
-
-class SubSection {
-
-    constructor() {
-        this.name = name;
-        this.rows = new Array();
-    }
-
-    add(el) {
-
-        if (el instanceof Row)
-            return this.addRow(el);
-        
-        else {
-
-            if (this.rows.length == 0 || el instanceof Input)
-                this.add(new Row());
-
-            if (el instanceof Input)
-                return this.rows[this.rows.length - 1].add(el);
-
-            return this.rows[0].add(el);
-        }
-    }
-
-    addRow(el) {
-        return this.rows.push(el);
-    }
-
-    row(id) {
-        // TODO
-    }
-}
-
-class Row {
-
-    constructor() {
-        this.columns = new Array();
-    }
-
-    add(el) {
-
-        if (el instanceof Column)
-            return this.addColumn(el);
-
-        else {
-
-            if (this.columns.length == 0)
-                this.add(new Column());
-
-            return this.columns[0].add(el);
-        }
-    }
-
-    addColumn(el) {
-        return this.columns.push(el);
-    }
-
-    column(id) {
-        // TODO
-    }
-}
-
-class Column {
-
-    constructor() {
-        this.inputs = new Array();
-    }
-
-    add(el) {
-
-        if (el instanceof Input)
-            return this.addInput(el);
-        else
+    add(ctrl)
+    {
+        if (ctrl instanceof Control)
+            this.controls.push(ctrl);
+        else 
             return false;
     }
+}
 
-    addInput(el) {
-        return this.inputs.push(el);
-    }
+class Control
+{
+    constructor(type, props)
+    {
+        this.properties = new Array();
 
-    input(id) {
-        // TODO
+        for (var i = 0; i < props.length; i++) 
+            this.properties.push(new Property(props[i]));
+
+        this.type = type;
     }
 }
 
-class Input {
+class Property
+{
+    constructor(args)
+    {
+        this.name = args.name; 
 
-    constructor(name = '') {
+        this.type = args.type;
+    }
+}
+
+class ArrayList
+{
+    constructor()
+    {
+        this.array = new Array();
+    }
+
+    get length()
+    {
+        return this.array.length;
+    }
+
+    add(el)
+    {
+        return this.array.push(el);
+    }
+
+    find(attrib, value)
+    {
+        for (var i = 0; i < this.array.length; i++) {
+            var el = this.array[i];
+
+            if (el[attrib] == value) 
+                return el;
+        }
+    }
+
+    get(i)
+    {
+        return this.array[i];
+    }
+}
+
+class Form
+{
+    constructor()
+    {
+        this.Pages = new ArrayList();
+    }
+
+    add(ctrl)
+    {
+        if (ctrl instanceof Page)
+            this.Pages.add(ctrl);
+
+        else 
+        {
+            if (this.Pages.length == 0)
+                this.Pages.add(new Page('Untitled Page'));
+
+            this.page(0).add(ctrl);
+        }
+    }
+
+    page(id)
+    {
+        return (typeof id == 'number') ? this.Pages.get(id) : this.Pages.find('name', id);
+    }
+}
+
+class Page
+{
+    constructor(name = '')
+    {
+        this.Sections = new ArrayList();
+
+        this.name = name;
+    }
+
+    add(ctrl)
+    {
+        if (ctrl instanceof Section)
+            this.Sections.add(ctrl);
+
+        else
+        {
+            if (this.Sections.length = 0)
+                this.Sections.add(new Section('Unnamed Section'))
+
+            this.section(0).add(ctrl);
+        }
+    }
+
+    section(id)
+    {
+        return (typeof id == 'number') ? this.Sections.get(id) : this.Sections.find('name', id);
+    }
+}
+
+class Section
+{
+    constructor(name = '')
+    {
         this.name = name;
     }
 }
